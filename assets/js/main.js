2bbf9d27145e655a3725d948736c08052efa1e43
mrphayo <anazodogodwin@gmail.com>
2019-05-29 14:16:19 +0100

    added the login functions and relevant stylings

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,10 +16,40 @@ function performPageActions(){
 
 function getEmployeesData(){
     queryServer("GET", "", server, "storeInfo");
+    
 }
+function btnEvents(){
+    $("form[name=login]").submit(function(e){
+        var authorized = "";
+        var password = "";
+        var employees = $("#empData").val();
+        var userEmail = $("#email").val();
+        var userPassword = $("#password").val();
+        employees = JSON.parse(employees);
+        employees.forEach(function(employee){
+            if(employee["email"] === userEmail){
+                authorized = employee["payroll-access"];
 
+                password = employee["password"];
+            }
+        });
+        if(authorized === ""){
+            $(".err").text("Email does not exist").removeClass("hide");
+        }else if(authorized === true){
+            if(userPassword === password){
+                window.location = "dashboard.html";
+            }else{
+                $(".err").text("Password Incorrect").removeClass("hide");
+            }
+        }else{
+            $(".err").removeClass("hide");
+        }
+        e.preventDefault();
+    });
+}
 function stashInfo(data){
-    alert(JSON.stringify(data));
+    var employeesData = JSON.stringify(data);
+    $("#empData").val(employeesData);
 }
 
 function linkFunction(callFunction, data){
